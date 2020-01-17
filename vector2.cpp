09b4427d92740af8b8612e9c52a7e86062f4608a
Sambastard71 <alvaro.sebastiano71@gmail.com>
2020-01-17 23:44:03 +0100

    added vector2 operator + overload

--- vector2.cpp
+++ vector2.cpp
@@ -1,3 +1,11 @@
+#include"vector2.h"
+
+vector2 & operator+ const (const int &vec2){
+    vector2 newVec;
+    newVec.x = this.x + vec2.x;
+    newVec.y = this.y + vec2.y;
+    return newVec;
+}
 
 int & get_x() const {
     return *x;
--- vector2.h
+++ vector2.h
@@ -2,6 +2,7 @@
 
 struct vector2 {
 				
+				vector2 & operator+(const int &vec2);
 				int & get_x() const;
 				int & get_y() const;
 				
